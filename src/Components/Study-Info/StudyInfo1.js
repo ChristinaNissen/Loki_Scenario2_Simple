088bc4b0c76e058d91496eaf7a20dd6334c2bc4e
Christina Nissen <christinaf.nissen@hotmail.com>
2026-04-04 15:23:46 +0200

    Update document with voting instructions

--- src/Components/Study-Info/StudyInfo1.js
+++ src/Components/Study-Info/StudyInfo1.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./study-info.css";
-import Instructions from "../../Assets/Instructions_e-voting.pdf";
+import Instructions from "../../Assets/Voting_System_Instructions.pdf";
 import { downloadFile } from "../../util";
 
 const StudyInfo1 = () => {
